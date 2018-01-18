0af8a0408a07ccd8448eae07e58a5e13e2904933
Corithm <chl-vcs@outlook.com>
2018-01-18 22:51:05 +0200

    Just in case.

---
+++ src/test/assemblerTest/AssemblerTest.java
@@ -0,0 +1,15 @@
+package assemblerTest;
+
+import main.assembler.Assembler;
+
+/**
+ * Test the Assembler.java.
+ */
+public class AssemblerTest
+{
+    public static void main(String args)
+    {
+        Assembler assembler = new Assembler();
+        assembler.compile("asmTest.asm");
+    }
+}
