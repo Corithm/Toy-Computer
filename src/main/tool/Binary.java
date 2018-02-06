76f31e7c556c2a1f561ecb17d1019c399b354ab4
Corithm <chl-vcs@outlook.com>
2018-02-06 18:06:46 +0200

    Rewrote Binary.java

--- src/main/tool/Binary.java
+++ src/main/tool/Binary.java
@@ -1,53 +1,83 @@
 package main.tool;
 
-import main.hardware.chip.combinational.ALU;
-
 /**
- * A class to represent a binary sequence.
+ * Represents a binary sequence.
+ *
+ * The values are stored as an array of boolean values.
  */
-public class Binary
+public final class Binary
 {
-    private final int LENGTH = 16;
-    private final boolean[] INCREMENT_CONTROL = Tools.toBinary("01 1111");
-    private final boolean[] DECREMENT_CONTROL = Tools.toBinary("00 1110");
+    private final int LENGTH;
+    private boolean[] sequence;
 
-    public boolean[] sequence = new boolean[LENGTH];
-
-    public Binary()
-    {
-        set("0000 0000 0000 0000");
+    /**
+     * The default constructor sets the LENGTH to 16.
+     */
+    public Binary() {
+        LENGTH = 16;
+        sequence = new boolean[16];
     }
 
+    /**
+     * Initialize a new Binary -sequence with a String object.
+     *
+     * @param sequence to save
+     */
     public Binary(String sequence)
     {
-        set(sequence);
-    }
+        LENGTH = sequence.length();
+        this.sequence = new boolean[LENGTH];
 
-    public Binary(int integer)
-    {
-        set(Tools.toBinaryString(integer));
+        set(sequence);
     }
 
     /**
      * Sets the string as binary.
      *
-     * @param sequence
+     * @param sequence to set
      */
     public final void set(String sequence)
     {
-        sequence = sequence.trim();
+        sequence = sequence.replaceAll("_", "");
+
         for (int i = 0; i < LENGTH; i++)
         {
             this.sequence[i] = (sequence.charAt(i) == 1);
         }
     }
 
+    /**
+     * Returns the binary sequence as boolean array.
+     *
+     * @return sequence as boolean[]
+     */
+    public final boolean[] getSequence() { return sequence; }
 
-    public final void decrement() {
-        Tools.alu.in(sequence, null, DECREMENT_CONTROL);
-    }
+    /**
+     * Returns the length of sequence.
+     *
+     * @return length as int
+     */
+    public final int getLength() { return LENGTH; }
 
-    public final void increment() {
-        Tools.alu.in(sequence, null, INCREMENT_CONTROL);
+    /**
+     * Returns the binary sequence as String.
+     * Use this as little as possible as this builds a String every call.
+     *
+     * @return sequence as String.
+     */
+    @Override public String toString()
+    {
+        StringBuilder result = new StringBuilder(LENGTH);
+
+        for (boolean bit : sequence)
+        {
+            if (bit)
+                result.append('1');
+            else
+                result.append('0');
+        }
+
+        return result.toString();
     }
 }
